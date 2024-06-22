073ac486c9f2de92b93cb3e62c220691a9be4509
Ayush <ayushpanchal005@gmail.com>
2024-06-22 20:11:15 +0530

    added the output component

---
+++ frontend/src/components/Output.tsx
@@ -0,0 +1,15 @@
+import { useSearchParams } from 'react-router-dom';
+
+export const Output = () => {
+  const [searchParams] = useSearchParams();
+  const replId = searchParams.get('replId') ?? '';
+
+  // domain name update
+  const URI = `http://localhost:3002`;
+
+  return (
+    <div className="bg-white h-[40vh]">
+      <iframe src={URI} height="100%" width="100%"></iframe>
+    </div>
+  );
+};
