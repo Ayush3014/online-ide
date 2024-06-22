e103f0e6ba9c76265fba9d422fe7725e2c7a6f1d
Ayush <ayushpanchal005@gmail.com>
2024-06-22 20:02:22 +0530

    added the editor ui

---
+++ frontend/src/components/Editor.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState, useMemo } from 'react';
+import Sidebar from './external/editor/components/sidebar';
+import { Code } from './external/editor/editor/code';
+import {
+  File,
+  buildFileTree,
+  RemoteFile,
+} from './external/editor/utils/file-manager';
+import { FileTree } from './external/editor/components/file-tree';
+import { Socket } from 'socket.io-client';
+
+export const Editor = ({
+  files,
+  onSelect,
+  selectedFile,
+  socket,
+}: {
+  files: RemoteFile[];
+  onSelect: (file: File) => void;
+  selectedFile: File | undefined;
+  socket: Socket;
+}) => {
+  const rootDir = useMemo(() => {
+    return buildFileTree(files);
+  }, [files]);
+
+  useEffect(() => {
+    if (!selectedFile) {
+      onSelect(rootDir.files[0]);
+    }
+  }, [selectedFile]);
+
+  return (
+    <div>
+      <main className="flex">
+        <Sidebar>
+          <FileTree
+            rootDir={rootDir}
+            selectedFile={selectedFile}
+            onSelect={onSelect}
+          />
+        </Sidebar>
+        <Code socket={socket} selectedFile={selectedFile} />
+      </main>
+    </div>
+  );
+};
