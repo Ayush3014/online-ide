21c927802a8f8cea648932ac10f21da282aad9e7
Ayush <ayushpanchal005@gmail.com>
2024-06-22 10:32:27 +0530

    added filemanager utils

---
+++ frontend/src/components/external/editor/utils/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { buildFileTree, Directory } from './file-manager';
+
+export const useFilesFromSandbox = (
+  id: string,
+  callback: (dir: Directory) => void
+) => {
+  useEffect(() => {
+    fetch('https://codesandbox.io/api/v1/sandboxes' + id)
+      .then((response) => response.json())
+      .then(({ data }) => {
+        const rootDir = buildFileTree(data);
+        callback(rootDir);
+      });
+  }, []);
+};
