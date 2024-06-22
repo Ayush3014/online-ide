ef66da6b1af7c54f1f2f98d3cf0110fde0a67023
Ayush <ayushpanchal005@gmail.com>
2024-06-22 21:21:44 +0530

    updated the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { CodingPage } from './components/CodingPage';
 import Landing from './components/Landing';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/coding" element={<CodingPage />} />
         <Route path="/" element={<Landing />} />
       </Routes>
     </BrowserRouter>
