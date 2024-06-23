b19fa6212f4a533be5f5c2ca46c5901dfb32cc17
Ayush <ayushpanchal005@gmail.com>
2024-06-23 15:18:00 +0530

    added terminalManager class for managing multiple terminal sessions

---
+++ runner/src/pty.ts
@@ -0,0 +1,73 @@
+// change => also can use spawn instead of fork if it doesn't work
+// @ts-ignore
+import { fork, IPty } from 'node-pty';
+import path from 'path';
+
+const SHELL = 'bash';
+
+export class TerminalManager {
+  private sessions: { [id: string]: { terminal: IPty; replId: string } } = {};
+
+  //   private sessions: { [id: string]: Session } = {};  // for spawn
+
+  constructor() {
+    this.sessions = {};
+  }
+
+  createPty(
+    id: string,
+    replId: string,
+    onData: (data: string, id: number) => void
+  ) {
+    let term = fork(SHELL, [], {
+      cols: 100,
+      name: 'xterm',
+      cwd: `/workspace`,
+    });
+
+    term.on('data', (data: string) => onData(data, term.pid));
+    this.sessions[id] = {
+      terminal: term,
+      replId,
+    };
+    term.on('exit', () => {
+      delete this.sessions[term.pid];
+    });
+    return term;
+  }
+
+  // for spawn
+  //   createPty(
+  //     id: string,
+  //     replId: string,
+  //     onData: (data: string, pid: number) => void
+  //   ): IPty {
+  //     const term = spawn(SHELL, [], {
+  //       cols: 100,
+  //       name: 'xterm',
+  //       cwd: `/workspace`,
+  //     });
+
+  //     term.on('data', (data: string) => onData(data, term.pid));
+
+  //     this.sessions[id] = {
+  //       terminal: term,
+  //       replId,
+  //     };
+
+  //     term.on('exit', () => {
+  //       delete this.sessions[id];
+  //     });
+
+  //     return term;
+  //   }
+
+  write(terminalId: string, data: string) {
+    this.sessions[terminalId]?.terminal.write(data);
+  }
+
+  clear(terminalId: string) {
+    this.sessions[terminalId].terminal.kill();
+    delete this.sessions[terminalId];
+  }
+}
