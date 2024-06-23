daa6d8476f84e4047904b921d844aea6a68c56e7
Ayush <ayushpanchal005@gmail.com>
2024-06-23 15:47:56 +0530

    create express and ws server

---
+++ runner/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { initWs } from './ws';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+
+initWs(httpServer);
+
+const port = process.env.PORT || 3001;
+httpServer.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
